69a003720635934d5ce61343fc24242410756981
KrokaKrola <harlamsan@gmail.com>
2026-01-12 14:54:55 +0200

    add String method for inner types

--- internal/resp/types.go
+++ internal/resp/types.go
@@ -1,7 +1,13 @@
 package resp
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Value interface {
 	isValue()
+	String() string
 }
 
 type SimpleString struct {
@@ -10,6 +16,10 @@ type SimpleString struct {
 
 func (s *SimpleString) isValue() {}
 
+func (s *SimpleString) String() string {
+	return string(s.Bytes)
+}
+
 type BulkString struct {
 	Bytes []byte
 	Null  bool
@@ -17,11 +27,19 @@ type BulkString struct {
 
 func (s *BulkString) isValue() {}
 
+func (s *BulkString) String() string {
+	return string(s.Bytes)
+}
+
 type Integer struct {
 	Number int64
 }
 
 func (s *Integer) isValue() {}
+
+func (s *Integer) String() string {
+	return fmt.Sprint(s.Number)
+}
 
 type Error struct {
 	Msg string
@@ -29,9 +47,27 @@ type Error struct {
 
 func (s *Error) isValue() {}
 
+func (s *Error) String() string {
+	return s.Msg
+}
+
 type Array struct {
 	Elements []Value
 	Null     bool
 }
 
 func (s *Array) isValue() {}
+
+func (s *Array) String() string {
+	if s.Null {
+		return "[]"
+	}
+
+	result := make([]string, 0, len(s.Elements))
+
+	for _, v := range s.Elements {
+		result = append(result, v.String())
+	}
+
+	return "[" + strings.Join(result, ",") + "]"
+}
